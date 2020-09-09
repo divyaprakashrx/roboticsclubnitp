e3e7ad34bc273bd4c0afb5fa069acc94e1d12e1d
Divya Prakash <medivyaprakash@gmail.com>
2020-09-09 22:07:45 +0530

    all designs updated

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.scss']
 })
 export class TeamComponent implements OnInit {
 
