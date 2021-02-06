dbbfef5e157a81751e3489d6294085e527d77fe4
Divya Prakash <medivyaprakash@gmail.com>
2021-02-06 12:13:37 +0530

    clean

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,6 +24,46 @@ export class HomeComponent implements OnInit {
 
   showNavigationArrows = false;
   showNavigationIndicators = true;
+  piabout = `It is my pleasure to introduce one of the most esteemed clubs of NIT
+            Patna, the Robotics Club. Formed in the year 2015 by a bunch of not
+            more than 8 students with vision of augmenting innovation and
+            technical culture in the college. In this short span of time,
+            Robotics Club managed to achieve an identity among college clubs at
+            State-level. With its members ranging from coders to designers to
+            hardware enthusiasts, they have been conducting an array of events
+            around the year, thus enhancing the students' technical skills and
+            making them aware of the fascinating world of automation, robotics,
+            and electronics surrounding them. They are also promoting robotics
+            among students of other nearby schools and colleges so that those
+            students who lack resources also get equal opportunity to explore
+            this fascinating domain. Members of this club have represented our
+            college in several inter-college technical competitions, ideathons,
+            and hackathons. The club has done a range of projects on IoT,
+            drones, 3D designing, Bot fabrication, etc. Presently, members are
+            actively engaged in working on a number of advanced projects like an
+            AI-based attendance system and an automated bot for restaurants.
+            Events like Robowars, Tiger's Nest, draws huge participation and
+            audience. The club has been pretty successful in creating an
+            atmosphere in the campus where people with different ideas could
+            collaborate together to give shape to their ideas. I hope that the
+            club continues doing so and keep making our college proud.`;
+
+  aboutus = `Robotics club has been embodiment of innovation at NIT Patna. Being formed in year 2015-16 by Nripendra Saroj Sir ,our club boasts a glorious legacy. With our members ranging from seasoned coders to brilliant designers to hardware scientists, Our members have also represented our college at Smart India hackathon,headed by our current Captain Dheeraj Kumar Sir.We have been constantly working to enhance the students’ technical skills and making them aware of the fascinating world of automation,robotics and electronics which surrounds us.Boasting of a huge roster of exciting and engaging events, This  year’s techno-cultural  fest was nothing less than a rollercoaster ride for us.`;
+  
+  whatwedo = `Does robots fascinates you and have you ever fantasized to build one
+            for yourself? The Robotics club NIT Patna is the place! The Robotics
+            club at NIT Patna strives to stimulate interest in robotics among
+            the students of the institute.Today robotics is a rapidly growing
+            field,as technological advances continue;researching,designing, and
+            building new robots serve various practical purposes.Robotics is a
+            field which is not limited to a particular branch and is way more
+            diverse than our own imagination and so is our student club.We are a
+            diverse group of robotics nerds who find roots across all academic
+            departments of the Institute. We at Robotics club believe in working
+            and growing together.You can know more about us from here. #Hail
+            Robotics.`;
+  
+  
   events = [
     {
       title: `Arduino workshop`,
@@ -77,12 +117,10 @@ export class HomeComponent implements OnInit {
       title: 'PCB designing',
       url: 'assets/img/pcb.jpg',
       description: `Printed Circuit Board are essential part of a electronic connections to generate a mechanical support as well. We can’t rely upon bread board for substantially increased number of components and for smaller packaging sizes of Integrated circuits .
-
-Robotics club ,NIT Patna organized a workshop on PCB Designing. This workshop focused over designing various kinds of electronic circuits and their use in making PCB Designing. Designing Software, Reference Materials, PPTs, Example sets and Study Materials were the tools provided (for the session).Concepts like :Circuit Designing, Simulation, PCB Design Structure, Component Placement and Design, Testing, Board Analysis, Fabrication output generation were discussed.`,
+      Robotics club ,NIT Patna organized a workshop on PCB Designing. This workshop focused over designing various kinds of electronic circuits and their use in making PCB Designing. Designing Software, Reference Materials, PPTs, Example sets and Study Materials were the tools provided (for the session).Concepts like :Circuit Designing, Simulation, PCB Design Structure, Component Placement and Design, Testing, Board Analysis, Fabrication output generation were discussed.`,
     },
   ];
-  aboutus = `Robotics club has been embodiment of innovation at NIT Patna. Being formed in year 2015-16 by Nripendra Saroj Sir ,our club boasts a glorious legacy. With our members ranging from seasoned coders to brilliant designers to hardware scientists, Our members have also represented our college at Smart India hackathon,headed by our current Captain Dheeraj Kumar Sir.We have been constantly working to enhance the students’ technical skills and making them aware of the fascinating world of automation,robotics and electronics which surrounds us.Boasting of a huge roster of exciting and engaging events, This  year’s techno-cultural  fest was nothing less than a rollercoaster ride for us.
-`;
+
   constructor(
     private breakpointObserver: BreakpointObserver,
     config: NgbCarouselConfig
