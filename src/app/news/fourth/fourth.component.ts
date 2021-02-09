e4b16c0c379114529e4321f850b4fef049b9826d
Mohd Zubair <55323065+Mohdzubair3056@users.noreply.github.com>
2021-02-09 21:54:49 +0530

    added fourth newsletter

---
+++ src/app/news/fourth/fourth.component.ts
@@ -0,0 +1,15 @@
+mport { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './fourth.component.html',
+  styleUrls: ['./fourth.component.scss']
+})
+export class FourthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
